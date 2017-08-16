d5e38bec2319fe753293827a656055f77841fd5f
Charlie Pitkin <charlie@wearefine.com>
2017-08-16 10:30:36 -0500

    fixed the rvm function was not setting the ruby_string correctly.

--- vars/rvm.groovy
+++ vars/rvm.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
 def call(String commands) {
-  if(env.BRANCH_NAME == 'production'){
-    ruby_string = "${env.RUBY_VERSION}@${env.RUBY_GEMSET}"
-  }
-  else if(env.BRANCH_NAME == /PR-.*/) {
+  if(env.BRANCH_NAME == /PR-.*/) {
     ruby_string = "${env.RUBY_VERSION}@pr-${env.RUBY_GEMSET}"
+  }
+  else {
+    ruby_string = "${env.RUBY_VERSION}@${env.RUBY_GEMSET}"
   }
   if (env.DEBUG == 'true') {
     echo "*************************"
